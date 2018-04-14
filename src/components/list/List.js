0dbc4f459550338406809a60305f7aaa57de9305
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-14 17:03:11 +0000

    Add pagination component

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -3,6 +3,7 @@ import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
 import Table from './Table';
+import Pagination from './Pagination';
 
 class List extends React.Component {
   constructor() {
@@ -11,19 +12,32 @@ class List extends React.Component {
     this.state = {
       loading: false,
       currencies: [],
+      totalPages: 0,
+      page: 1,
       error: null // 'Something is wrong'
     }
+
+    // allow child component to edit state inside this component (parent)
+    this.handlePaginationClick = this.handlePaginationClick.bind(this);
   }
 
   componentDidMount() {
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies() {
     this.setState({loading: true});
 
-    fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+    const { page } = this.state;
+
+    fetch(`${API_URL}/cryptocurrencies?page=${page}&perPage=20`)
       .then(handleResponse)
       .then((data) => {
+        const {currencies, totalPages} = data;
         this.setState({
-          currencies: data.currencies,
-          loading: false
+          currencies: currencies,
+          loading: false,
+          totalPages: totalPages
         });
       })
       .catch((error) => {
@@ -44,12 +58,28 @@ class List extends React.Component {
     }
   }
 
+  handlePaginationClick(direction) {
+    // console.log(this);
+
+    let nextPage = this.state.page;
+
+    if (direction === 'next') {
+      nextPage++;
+    } else {
+      nextPage--;
+    }
+
+    this.setState({ page: nextPage }, () => {
+      this.fetchCurrencies();
+    });
+  }
+
   render() {
     console.log(this.state);
 
     // ES6 Destructuring
     // https://www.saltycrane.com/blog/2016/03/es6-features-used-react-development/#destructuring
-    const {loading, error, currencies} = this.state;
+    const {loading, error, currencies, page, totalPages} = this.state;
 
     if (loading) {
       return <div className="loading-container"><Loading /></div>
@@ -60,10 +90,18 @@ class List extends React.Component {
     }
 
     return (
-      <Table
-        coins={currencies}
-        renderChangePercent={this.renderChangePercent}
+      <div>
+        <Table
+          coins={currencies}
+          renderChangePercent={this.renderChangePercent}
+        />
+
+      <Pagination
+        page={page}
+        totalPages={totalPages}
+        handlePaginationClick={this.handlePaginationClick}
       />
+      </div>
     )
   }
 }
